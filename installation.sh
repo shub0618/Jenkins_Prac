1e0b971aa0fdfb70e571b9c09b4dd604f96d31dc
shubham <shubhamshetkar46@gmail.com>
2024-12-22 10:58:13 +0530

    update

--- installation.sh
+++ installation.sh
@@ -9,17 +9,18 @@ sudo apt update
 #Update the Debian apt repositories, install OpenJDK 17, and check the installation with the commands:
 
 #install the java dependencies
-sudo apt install fontconfig openjdk-17-jre
 
-# to check if the java has been installed or not 
-java -version
-#openjdk version "17.0.8" 2023-07-18
-#OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
-#OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+sudo apt install openjdk-21-jdk -y
+sudo apt install openjdk-17-jdk -y
+
+# sudo apt install fontconfig openjdk-17-jre
+
+# check if the java has been installed or not 
+# java --version
 
 # Run this commands to install the jenkins repository and to generate the key 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key -y
   
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
@@ -36,23 +37,21 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 #Set Jenkins to listen on port **8080**. Access this port with your browser to start configuration.  
 
 #If Jenkins fails to start because a port is in use, run systemctl edit jenkins and add the following:
-
 #[Service]
 #Environment="JENKINS_PORT=8081"
-
 #Here, "8081" was chosen but you can put another port available.
   
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 #Why use **apt** and not **apt-get** or another command? The apt command has been available since 2014. 
 #It has a command structure that is similar to **apt-get** but was created to be a more pleasant experience for typical users. 
 #Simple software management tasks like install, search and remove are easier with **apt**.
 
-sudo systemctl enable jenkins
+# sudo systemctl enable jenkins
 
-sudo systemctl start jenkins
+# sudo systemctl start jenkins
 
-sudo systemctl status jenkins
+# sudo systemctl status jenkins
 
